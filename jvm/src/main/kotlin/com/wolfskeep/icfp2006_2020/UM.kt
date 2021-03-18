6a8fcb26b0a4a6b8fa10d713d6948f4f26716527
T. Alexander Popiel <tapopiel@gmail.com>
2021-03-18 16:08:34 -0700

    Initial attempt at jvm version

---
+++ jvm/src/main/kotlin/com/wolfskeep/icfp2006_2020/UM.kt
@@ -0,0 +1,84 @@
+package com.wolfskeep.icfp2006_2020
+
+import java.io.*
+import org.jline.terminal.*
+
+fun main(args: Array<String>) {
+    val file = File(args[0])
+    if (!file.canRead()) {
+        println("Cannot read file ${args[0]}")
+        System.exit(1)
+    }
+    val a0 = IntArray(fs.length() / 4)
+    FileInputStream(file).use { fs ->
+        val ds = DataInputStream(BufferedInputStream(fs))
+        for (j in 0..(a0.length - 1)) a0[j] = ds.readInt()
+    }
+
+    UM(a0).run()
+}
+
+class UM(a0: IntArray) {
+    val blank = IntArray(0)
+    val registers = IntArray(8)
+    val arrays = mutableListOf(a0)
+    val available = ArrayDeque<Int>()
+    var finger = 0
+    var operator = 0
+
+    var A: Int
+        inline get() = registers[(operator shr 6) and 7]
+        inline set(value) = registers[(operator shr 6) and 7] = value
+    var B: Int
+        inline get() = registers[(operator shr 3) and 7]
+        inline set(value) = registers[(operator shr 3) and 7] = value
+    var C: Int
+        inline get() = registers[(operator shr 0) and 7]
+        inline set(value) = registers[(operator shr 0) and 7] = value
+    var D: Int
+        inline get() = registers[(operator shr 25) and 7]
+        inline set(value) = registers[(operator shr 25) and 7] = value
+    val V: Int
+        inline get() = operator and 0x01FFFFFF
+
+    fun run() {
+        TerminalBuilder.terminal().use { terminal ->
+            terminal.enterRawMode()
+            terminal.echo(true)
+            val input = terminal.input()
+            val output = terminal.output()
+    
+            while (true) {
+                operator = arrays[0][finger]
+                finger += 1
+                when (operator ushr 24) {
+                    0 -> if (C != 0) A = B
+                    1 -> A = arrays[B][C]
+                    2 -> arrays[A][B] = C
+                    3 -> A = B + C
+                    4 -> A = (B.toUInt * C.toUInt).toInt
+                    5 -> A = (B.toUInt / C.toUInt).toInt
+                    6 -> A = inv(B and C)
+                    7 -> return
+                    8 ->
+                        val fresh = if (C == 0) blank else IntArray(C)
+                        if (available.size() == 0) {
+                            B = arrays.length
+                            arrays += fresh
+                        } else {
+                            B = available.pop()
+                            arrays[B] = fresh
+                        }
+                    9 -> arrays[C] = blank; available.push(C)
+                    10 -> output.write(C); terminal.flush()
+                    11 -> try { C = input.read() } catch (e: EOFException) { C = -1 }
+                    12 ->
+                        if (B != 0) arrays[0] = arrays[B].clone()
+                        finger = C
+                    13 -> D = V
+                    else -> println("Illegal operation"); System.exit(1)
+                }
+            }
+        }
+    }
+}
