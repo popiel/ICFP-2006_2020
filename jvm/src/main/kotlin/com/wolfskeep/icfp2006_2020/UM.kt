dfe7e38399586a0973186f3408e74aad707f94b4
T. Alexander Popiel <tapopiel@gmail.com>
2021-03-19 19:34:08 -0700

    Saving and reloading works in jvm

--- jvm/src/main/kotlin/com/wolfskeep/icfp2006_2020/UM.kt
+++ jvm/src/main/kotlin/com/wolfskeep/icfp2006_2020/UM.kt
@@ -1,6 +1,7 @@
 package com.wolfskeep.icfp2006_2020
 
 import java.io.*
+import java.time.*
 import org.jline.terminal.*
 
 fun main(args: Array<String>) {
@@ -9,22 +10,64 @@ fun main(args: Array<String>) {
         println("Cannot read file ${args[0]}")
         System.exit(1)
     }
-    val a0 = IntArray(file.length().toInt() / 4)
-    FileInputStream(file).use { fs ->
-        val ds = DataInputStream(BufferedInputStream(fs))
-        for (j in 0..(a0.size - 1)) a0[j] = ds.readInt()
+    val um = DataInputStream(BufferedInputStream(FileInputStream(file))).use { ds ->
+        val first = ds.readInt()
+        if ((first ushr 28) == 14) {
+            loadState(ds, first)
+        } else {
+            val a0 = IntArray(file.length().toInt() / 4)
+            a0[0] = first
+            for (j in 1..(a0.size - 1)) a0[j] = ds.readInt()
+            UM(a0)
+        }
     }
 
-    UM(a0).run()
+    um.run()
 }
 
-class UM(a0: IntArray) {
-    val blank = IntArray(0)
+fun loadState(ds: DataInputStream, first: Int): UM {
+    val finger = ds.readInt()
     val registers = IntArray(8)
-    val arrays = mutableListOf(a0)
+    for (j in 0..7) registers[j] = ds.readInt()
+    val outBuffer = ByteArray(256)
+    ds.readFully(outBuffer)
+    val arrays = ArrayList<IntArray>()
+    for (j in 0 until (first and ((1 shl 28) - 1))) {
+        val arr = IntArray(ds.readInt())
+        for (k in 0 until arr.size) arr[k] = ds.readInt()
+        arrays += arr
+    }
     val available = ArrayDeque<Int>()
-    var finger = 0
+    for (j in 0 until ds.readInt()) available.add(ds.readInt())
+
+    print(if (outBuffer[0].toInt() == 0) {
+        outBuffer.dropWhile { it.toInt() == 0 }
+    } else {
+        outBuffer.dropWhile { it.toInt() != 10 }
+    }.joinToString("") { it.toChar().toString() })
+    System.out.flush()
+
+    return UM(arrays, available, registers, finger, outBuffer)
+}
+
+class UM(
+    val arrays: MutableList<IntArray>,
+    val available: ArrayDeque<Int>,
+    val registers: IntArray,
+    var finger: Int,
+    val outBuffer: ByteArray
+) {
+    constructor(a0: IntArray): this(
+        mutableListOf(a0),
+        ArrayDeque<Int>(),
+        IntArray(8),
+        0,
+        ByteArray(256)
+    ) {}
+
+    val blank = IntArray(0)
     var operator = 0
+    var outPos = 0
 
     var A: Int
         inline get() = registers[(operator shr 6) and 7]
@@ -72,8 +115,29 @@ class UM(a0: IntArray) {
                         }
                     }
                     9 -> { arrays[C] = blank; available.add(C) }
-                    10 -> { output.write(C); terminal.flush() }
-                    11 -> try { C = input.read() } catch (e: EOFException) { C = -1 }
+                    10 -> {
+                        output.write(C)
+                        outBuffer[outPos] = C.toByte()
+                        outPos = (outPos + 1) % outBuffer.size
+                        terminal.flush()
+                    }
+                    11 -> try {
+                        val c = input.read()
+                        if (c == 7) {
+                            dumpState()
+                            finger -= 1
+                        } else if (c == 13) {
+                            output.write(10)
+                            terminal.flush()
+                            C = 10
+                            outBuffer[outPos] = C.toByte()
+                            outPos = (outPos + 1) % outBuffer.size
+                        } else {
+                            C = c
+                            outBuffer[outPos] = C.toByte()
+                            outPos = (outPos + 1) % outBuffer.size
+                        }
+                    } catch (e: EOFException) { C = -1 }
                     12 -> {
                         if (B != 0) arrays[0] = arrays[B].clone()
                         finger = C
@@ -83,5 +147,22 @@ class UM(a0: IntArray) {
                 }
             }
         }
+    }
+
+    fun dumpState() {
+        val name = String.format("umDump-%1\$tY-%1\$tm-%1\$td_%1\$tH-%1\$tM-%1\$tS.um", LocalDateTime.now())
+        println()
+        println("Saving $name")
+        DataOutputStream(BufferedOutputStream(FileOutputStream(name))).use { out ->
+            out.writeInt((14 shl 28) + arrays.size)
+            out.writeInt(finger - 1)
+            registers.forEach(out::writeInt)
+            out.write(outBuffer, outPos, outBuffer.size - outPos)
+            out.write(outBuffer, 0, outPos)
+            for (j in arrays) { out.writeInt(j.size); j.forEach(out::writeInt) }
+            out.writeInt(available.size)
+            available.forEach(out::writeInt)
+        }
+        println("Saved")
     }
 }
