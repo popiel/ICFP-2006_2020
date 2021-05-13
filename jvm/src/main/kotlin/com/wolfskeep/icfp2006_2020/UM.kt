3e80eddbda15651992dd0e1fbb4c5341c30be5ee
T. Alexander Popiel <tapopiel@gmail.com>
2021-05-12 17:02:01 -0700

    Augment with ability to trace

--- jvm/src/main/kotlin/com/wolfskeep/icfp2006_2020/UM.kt
+++ jvm/src/main/kotlin/com/wolfskeep/icfp2006_2020/UM.kt
@@ -7,7 +7,12 @@ import org.jline.reader.*
 import org.jline.terminal.*
 
 fun main(args: Array<String>) {
-    val file = File(args[0])
+    val traceArgs = args.filter { it.startsWith("-t") }.map { it.toLowerCase() }
+    val traceFrag = traceArgs.any { "-tfragments".startsWith(it) }
+    val traceSet  = traceArgs.any { "-tset"      .startsWith(it) }
+    val traceOp   = traceArgs.any { "-toperators".startsWith(it) }
+
+    val file = File(args.find { it[0] != '-' })
     if (!file.canRead()) {
         println("Cannot read file ${args[0]}")
         System.exit(1)
@@ -15,19 +20,19 @@ fun main(args: Array<String>) {
     val um = DataInputStream(BufferedInputStream(FileInputStream(file))).use { ds ->
         val first = ds.readInt()
         if ((first ushr 28) == 14) {
-            loadState(ds, first)
+            loadState(ds, first, traceFrag, traceSet, traceOp)
         } else {
             val a0 = IntArray(file.length().toInt() / 4)
             a0[0] = first
             for (j in 1..(a0.size - 1)) a0[j] = ds.readInt()
-            UM(a0)
+            UM(a0, traceFrag, traceSet, traceOp)
         }
     }
 
     um.run()
 }
 
-fun loadState(ds: DataInputStream, first: Int): UM {
+fun loadState(ds: DataInputStream, first: Int, traceFrag: Boolean, traceSet: Boolean, traceOp: Boolean): UM {
     val finger = ds.readInt()
     val registers = IntArray(8)
     for (j in 0..7) registers[j] = ds.readInt()
@@ -49,7 +54,33 @@ fun loadState(ds: DataInputStream, first: Int): UM {
     }.joinToString("") { it.toChar().toString() })
     System.out.flush()
 
-    return UM(arrays, available, registers, finger, outBuffer)
+    return UM(arrays, available, registers, finger, outBuffer, traceFrag, traceSet, traceOp)
+}
+
+fun decode(operator: Int): String {
+    val AN: String = "abcdefgh"[operator shr 6 and 7].toString()
+    val BN: String = "abcdefgh"[operator shr 3 and 7].toString()
+    val CN: String = "abcdefgh"[operator shr 0 and 7].toString()
+    val DN: String = "abcdefgh"[operator shr 25 and 7].toString()
+    return when (operator ushr 28) {
+        0 -> "IF ($CN) $AN = $BN"
+        1 -> "$AN = arrays[$BN][$CN]"
+        2 -> "arrays[$AN][$BN] = $CN"
+        3 -> "$AN = $BN + $CN"
+        4 -> "$AN = $BN * $CN"
+        5 -> "$AN = $BN / $CN"
+        6 -> "$AN = $BN nand $CN"
+        7 -> "EXIT"
+        8 -> "$BN = NEW $CN"
+        9 -> "FREE $CN"
+        10 -> "OUT $CN"
+        11 -> "IN $CN"
+        12 -> "JUMP [$BN][$CN]"
+        13 -> "$DN = ${operator and 0x1ffffff}"
+        14 -> "SetRegisters"
+        15 -> "LoadRegister $CN"
+        else -> "Illegal"
+    }
 }
 
 class UM(
@@ -57,14 +88,20 @@ class UM(
     val available: ArrayDeque<Int>,
     val registers: IntArray,
     var finger: Int,
-    val outBuffer: ByteArray
+    val outBuffer: ByteArray,
+    val traceFrag: Boolean = false,
+    val traceSet:  Boolean = false,
+    val traceOp:   Boolean = false
 ) {
-    constructor(a0: IntArray): this(
+    constructor(a0: IntArray, traceFrag: Boolean, traceSet: Boolean, traceOp: Boolean): this(
         mutableListOf(a0),
         ArrayDeque<Int>(),
         IntArray(8),
         0,
-        ByteArray(256)
+        ByteArray(256),
+        traceFrag,
+        traceSet,
+        traceOp
     ) {}
 
     val blank = IntArray(0)
@@ -97,17 +134,23 @@ class UM(
 
             val output = terminal.output()
             var pendingLine = ""
+
+            var a0 = arrays[0]
     
             while (true) {
-                operator = arrays[0][finger]
+                operator = a0[finger]
                 // println("finger: $finger  operator: ${java.lang.Integer.toHexString(operator)}")
+                if (traceOp) System.err.println("$finger: ${decode(operator)}")
                 finger += 1
                 when (operator ushr 28) {
                     0 -> if (C != 0) A = B
                     1 -> A = arrays[B][C]
-                    2 -> arrays[A][B] = C
+                    2 -> {
+                        arrays[A][B] = C
+                        if (traceSet) System.err.println("TRACE: Setting $A[$B] = $C")
+                    }
                     3 -> A = B + C
-                    4 -> A = (B.toUInt() * C.toUInt()).toInt()
+                    4 -> A = B * C
                     5 -> A = (B.toUInt() / C.toUInt()).toInt()
                     6 -> A = (B and C).inv()
                     7 -> return@run
@@ -138,8 +181,12 @@ class UM(
                         pendingLine = pendingLine.drop(1)
                     } catch (e: EOFException) { C = -1 }
                     12 -> {
-                        if (B != 0) arrays[0] = arrays[B].clone()
+                        if (B != 0) {
+                            arrays[0] = arrays[B].clone()
+                            a0 = arrays[0]
+                        }
                         finger = C
+                        if (traceFrag) System.err.println("TRACE: Starting fragment at $finger with [${registers.joinToString(", ")}]")
                     }
                     13 -> D = V
                     else -> { println("Illegal operation ${operator ushr 28}"); System.exit(1) }
