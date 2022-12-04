90a497e3f8779d73097f06e73a07686f15062c4b
T. Alexander Popiel <tapopiel@gmail.com>
2022-12-04 01:02:49 -0800

    Making pithier advice

--- puzzles/hmonk/run.sh
+++ puzzles/hmonk/run.sh
@@ -3,16 +3,24 @@
 hmonk
 COMEFROM
 /bin/umodem arith2.adv STOP
-Add Z y => y;
-Add (S x) y => S (Add x y);
-{ Add (Add x y) z => Add x (Add y z); }
-Mult Z y => Z;
-Mult (S x) y => Add y (Mult x y);
-{ Mult (Mult x y) z => Mult x (Mult y z); }
-op (op x y) z => op x (op y z);
-Mult (Add x y) z => Add (Mult x z) (Mult y z);
+Add x Z => x;
+Add x (S y) => S (Add x y);
+Mult x Z => Z;
+Mult x (S y) => Add (Mult x y) x;
+op x (op y z) => op (op x y) z;
+Mult x (Add y z) => Add (Mult x y) (Mult x z);
+Compute x => x;
 DUP z z => z;
 x y z => x y (DUP z z);
+.
+STOP
+/bin/umodem arith3.adv STOP
+Compute (Add x Z) => Compute x;
+Compute (Add x (S y)) => S (Compute (Add x y));
+Compute (Mult x Z) => Compute Z;
+Compute (Mult x (S y)) => Compute (Add (Mult x y) x);
+Compute (x y z) => x y (Compute z);
+x (Compute y) => Compute (x y);
 Compute x => x;
 .
 STOP
@@ -27,7 +35,8 @@ Compute (Mult (Add (S (S Z)) (S (S (S Z)))) (Add (S (S (S Z))) (S (S Z)))) ->
 Z)))))))))))))))))))))))));
 .
 STOP
-advise arith arith2.adv arith2.tests
+advise arith arith3.adv arith2.tests
+mail
 exit
 FOO
 ) |
